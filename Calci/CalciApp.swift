e58ea2fb5243d9e3546e655a44ce7cc28eedcf41
Aryan Shaikh <aryan.shaikh@rock-solid.in>
2025-12-02 16:15:01 +0530

    Initial Commit

---
+++ Calci/CalciApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalciApp.swift
+//  Calci
+//
+//  Created by Aryan Shaikh on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CalciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calci/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calci
+//
+//  Created by Aryan Shaikh on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
