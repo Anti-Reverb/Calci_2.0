9738b177e6c1601cfb2ac03964846939281426bc
Aryan Shaikh <aryan.shaikh@rock-solid.in>
2025-12-03 11:31:34 +0530

    Adding UI Elements and improving UI

--- Calci/ContentView.swift
+++ Calci/ContentView.swift
@@ -12,16 +12,58 @@ struct ContentView: View {
     @AppStorage("streak") var streak = 0
     
     var body: some View {
-        VStack {
-            Text(String(streak))
+        
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
             
-            Button(action: {
-                streak += 1
-            },label: {
-                Text("+1")
-            })
+            VStack {
+                
+                Spacer()
+                
+                ZStack {
+                    Circle()
+                        .stroke(Color.white.opacity(0.1), lineWidth: 20)
+                    
+                    Circle()
+                        .trim(from: 0, to: 0.34)
+                        .stroke(.blue, style:
+                                    StrokeStyle(lineWidth: 20,
+                                                lineCap: .round,
+                                                lineJoin: .round))
+                        .rotationEffect(.degrees(-90))
+                    
+                    VStack {
+                        Text("Streak")
+                            .font(.system(size: 30))
+                        Text(String(streak))
+                            .font(.system(size: 80))
+                    }
+                    .foregroundStyle(.white)
+                    .fontDesign(.rounded)
+                    
+                }
+                .padding(.horizontal, 50)
+                
+                Spacer()
+                
+                Button(action: {
+                    streak += 1
+                },label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundStyle(.blue)
+                            .frame(height: 50)
+                        Text("+1")
+                            .foregroundStyle(.white )
+                    }
+                })
+                                
+            }
+            .padding()
+            .foregroundStyle(.blue)
         }
-        .padding()
+        
     }
 }
 
